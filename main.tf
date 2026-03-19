831b575e1270f38ed90d79f720c57962b164d12d
K <39096245+KishtheBeast@users.noreply.github.com>
2026-03-19 09:43:47 -0400

    Initial content

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "private_service" {
+  source = "github.com/jig-test-custome/test-tf-modules?ref=v1.0.0"
+  name   = "my-service"
+}
+
+module "another_service" {
+  source = "git::https://github.com/jig-test-custome/test-tf-modules.git?ref=old-branch"
+}
